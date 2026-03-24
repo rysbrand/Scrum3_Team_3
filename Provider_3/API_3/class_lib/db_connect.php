0294d469a6e741238851af24647976611b0f59b7
rysbrand <becca.leach325@gmail.com>
2026-03-24 13:15:48 -0500

    password shenanigans

--- Provider_3/API_3/class_lib/db_connect.php
+++ Provider_3/API_3/class_lib/db_connect.php
@@ -5,8 +5,6 @@ class DB_Access
     private static $hostName = "localhost";
     private static $databaseName = "study_service";
     private static $userName = "root";
-    private static $password = ""; //For if you have no password on php myadmin
-	//private static $password = "thing"; // For Rylee
     private static $password = "";
     private static $conn;
 
