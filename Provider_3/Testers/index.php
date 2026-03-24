04888333da71744aec9aa8749b1a64ac2de71d50
Rylee Smith <rylee.smith16@outlook.com>
2026-03-24 15:26:17 -0500

    Last testers implemented with maybe more work needed but the option is there for more testers to be used here.

--- Provider_3/Testers/index.php
+++ Provider_3/Testers/index.php
@@ -3,7 +3,7 @@ include('../API_3/class_lib/db_connect.php');
 
 $DB_Access = new DB_Access(); 
 
-$DB_Result = $DB_Access->displayRecords("projects"); //This API Tester only shows tables
+$DB_Result = $DB_Access->displayRecords("projects");
 $rValue = "";
 		while($row = $DB_Result->fetch_assoc())
 		{ 
@@ -17,7 +17,38 @@ $data['tablesData'] = $rValue; //"TestValue";
 
 print(json_encode($data));
 
+print("<br/>");
 
+$DB_Result = $DB_Access->displayRecords("tasks"); 
+$rValue = "";
+		while($row = $DB_Result->fetch_assoc())
+		{ 
+			foreach($row as $value)
+			{
+				$rValue = $rValue . "$value ";
+			}
+		}
+$data = array();
+$data['tablesData'] = $rValue; //"TestValue";
+
+print(json_encode($data));
+
+print("<br/>");
+$DB_Result = $DB_Access->displayRecords("users"); 
+$rValue = "";
+		while($row = $DB_Result->fetch_assoc())
+		{ 
+			foreach($row as $value)
+			{
+				$rValue = $rValue . "$value ";
+			}
+		}
+$data = array();
+$data['tablesData'] = $rValue; //"TestValue";
+
+print(json_encode($data));
+
+print("<br/>");
 
 
 ?>
