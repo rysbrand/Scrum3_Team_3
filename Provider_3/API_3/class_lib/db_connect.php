dc229e95fa5bd158fa7a4211fae57923cfd430e4
rysbrand <becca.leach325@gmail.com>
2026-03-24 10:39:38 -0500

    db connect class

--- Provider_3/API_3/class_lib/db_connect.php
+++ Provider_3/API_3/class_lib/db_connect.php
@@ -1,18 +1,23 @@
-<?php 
- 
-$localhost = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "study_service"; 
- 
-// create connection 
-$connect = new mysqli($localhost, $username, $password, $dbname); 
- 
-// check connection 
-if($connect->connect_error) {
-    die("connection failed : " . $connect->connect_error);
-} else {
-     //echo "Successfully Connected";
+<?php
+
+class DB_Access
+{
+    private static $conn;
+	private static $hostName = "localhost";
+	private static $databaseName = "study_service";
+	private static $userName = "root";
+	private static $password = "";
+
+    public function connectTo()
+    {
+		//self::$databaseName = $databaseName; //Assigned above instead of passed to the method
+		self::$conn = new mysqli(self::$hostName, self::$userName, self::$password, self::$databaseName );
+		if (self::$conn->connect_error) {
+			return("Connect Error to " . self::$hostName); //return error condition
+        }
+		return("Connection successful to hostName = " . self::$hostName . ", databaseName = " . self::$databaseName); //return success statement
+   				
+	}
 }
- 
+
 ?>
