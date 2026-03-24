d929fe038055550fca67a7ec2b2f8b0a01f38624
Rylee Smith <rylee.smith16@outlook.com>
2026-03-24 12:57:01 -0500

    Added a password variable for myself and added comments to the db_connect.php.

--- Provider_3/API_3/class_lib/db_connect.php
+++ Provider_3/API_3/class_lib/db_connect.php
@@ -5,7 +5,8 @@ class DB_Access
     private static $hostName = "localhost";
     private static $databaseName = "study_service";
     private static $userName = "root";
-    private static $password = "";
+    private static $password = ""; //For if you have no password on php myadmin
+	//private static $password = "thing"; // For Rylee
 
    public function connectTo()
 	{
