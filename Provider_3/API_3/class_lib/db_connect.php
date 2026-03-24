6d4f4fc05d7f142c24387a69262ef1139322b0b7
rysbrand <becca.leach325@gmail.com>
2026-03-24 11:53:37 -0500

    work in progress on CRUD

--- Provider_3/API_3/class_lib/db_connect.php
+++ Provider_3/API_3/class_lib/db_connect.php
@@ -2,22 +2,25 @@
 
 class DB_Access
 {
-    private static $conn;
-	private static $hostName = "localhost";
-	private static $databaseName = "study_service";
-	private static $userName = "root";
-	private static $password = "";
+    private static $hostName = "localhost";
+    private static $databaseName = "study_service";
+    private static $userName = "root";
+    private static $password = "";
 
-    public function connectTo()
-    {
-		
-		self::$conn = new mysqli(self::$hostName, self::$userName, self::$password, self::$databaseName );
-		if (self::$conn->connect_error) {
-			return("Connect Error to " . self::$hostName);
-        }
-		return("Connection successful to hostName = " . self::$hostName . ", databaseName = " . self::$databaseName);
-   				
+   public function connectTo()
+	{
+		$conn = new mysqli(
+			self::$hostName,
+			self::$userName,
+			self::$password,
+			self::$databaseName
+		);
+
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+
+		return $conn;
 	}
 }
-
 ?>
--- Provider_3/classes/CRUD.php
+++ Provider_3/classes/CRUD.php
@@ -1,68 +1,69 @@
 <?php
-require_once 'db_connect.php';
+require_once './../API_3/class_lib/db_connect.php';
 
-public function createUser() {
+$db = new Db_Access();
+
+function createUser() {
     if($_POST) {
 	$first_name = $_POST['fname'];
 	$last_name = $_POST['lname'];
 	$email = $_POST['email'];
 	$role = $_POST['role'];
 
-	$sql = "INSERT INTO users (fname, lname, email, role) VALUES ('$fname', '$lname', '$email', '$role')";
-	if($connect->query($sql) === TRUE) {
-		echo "<p>New User Successfully Created</p>";
-		echo "<a href='../Testers/index.phpp'><button type='button'>Home</button></a>";
-	} else {
-		echo "Error " . $sql . ' ' . $connect->connect_error;
+
+	$sql = "INSERT INTO users (fname, lname, email, role) VALUES ('$first_name', '$last_name', '$email', '$role')";
 	}
-
-	$connect->close();
-}
 }
 
-public function createTasks() {
+function createTask() {
+
     if($_POST) {
 	$title = $_POST['title'];
 	$description = $_POST['description'];
 	$status = $_POST['status'];
 	$priority = $_POST['priority'];
-    $due_date = $_POST['due']
+    $due_date = $_POST['due'];
 
 	$sql = "INSERT INTO tasks (title, description, status, priority, due_date) VALUES ('$title', '$description', '$status', '$priority', '$due_date')";
-	if($connect->query($sql) === TRUE) {
-		echo "<p>New task Successfully Created</p>";
-		echo "<a href='../Testers/index.phpp'><button type='button'>Home</button></a>";
-	} else {
-		echo "Error " . $sql . ' ' . $connect->connect_error;
-	}
-
-	$connect->close();
 }
 }
 
-public function createProjects() {
-    if($_POST) {
-	$category = $_POST['category'];
-	$status = $_POST['status'];
-	$due_date = $_POST['due'];
 
-	$sql = "INSERT INTO projects (category, status, due_date) VALUES ('$category', '$status', '$due_date')";
-	if($connect->query($sql) === TRUE) {
-		echo "<p>New project Successfully Created</p>";
-		echo "<a href='../Testers/index.phpp'><button type='button'>Home</button></a>";
-	} else {
-		echo "Error " . $sql . ' ' . $connect->connect_error;
-	}
 
-	$connect->close();
+function createProject()
+{
+    if ($_POST) {
+        $category = $_POST['category'];
+        $status = $_POST['status'];
+        $due_date = $_POST['due'];
+
+        $db = new DB_Access();
+        $connect = $db->connectTo();
+
+        $sql = "INSERT INTO projects (category, status, due_date)
+                VALUES ('$category', '$status', '$due_date')";
+
+        if ($connect->query($sql) === TRUE) {
+            echo "<p>New project successfully created</p>";
+            echo "<a href='../Testers/index.php'><button type='button'>Home</button></a>";
+        } else {
+            echo "Error: " . $sql . " " . $connect->error;
+        }
+
+        $connect->close();
+    }
 }
-}
 
-public function delete() {
+
+
+function delete() {
     if($_POST) {
 	$id = $_POST['id'];
-
-	$sql = "DELETE FROM users WHERE id = {$id}";
+    $column_name = $_POST['table_name']; // Make sure this is not the plural version
+    $column_id_name = $column_name . '_id'; // For the where clause
+    $column_FROM = $column . 's'; // For the from clause
+    $sql = "DELETE FROM {$column_FROM} WHERE {$column_id_name} = {$id}"; // I have no idea if this works test pls
+	//$sql = "DELETE FROM users WHERE id = {$id}";
 	if($connect->query($sql) === TRUE) {
 		echo "<p>Project Successfully removed!!</p>";
 		echo "<a href='../Testers/index.php'><button type='button'>Back</button></a>";
@@ -74,7 +75,7 @@ public function delete() {
 }
 }
 
-public function updateUser() {
+function updateUser() {
     if($_POST) {
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
